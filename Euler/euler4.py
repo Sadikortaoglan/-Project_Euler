00e9f0734ef29e77e812ab5b6a9c901d26c8c411
Onelife <ortcellad@gmail.com>
2020-12-13 14:39:33 +0300

    Değişken adı düzeltildi

--- Euler/euler4.py
+++ Euler/euler4.py
@@ -2,7 +2,7 @@ toplam=[]
 sum1=0
 for i in range(1,101):
      sum1+=i
-     a=i**2
-     toplam.append(a)
+     kare=i**2
+     toplam.append(kare)
 print(f"Karelerin toplamı :{sum(toplam)} ,Toplam değerlerin kareleri : {sum1**2}")
 print(f"Fark : {(sum1**2)-sum(toplam)}")
